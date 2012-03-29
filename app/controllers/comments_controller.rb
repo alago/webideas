f9573000cd82b03ee4c6071299fad258db174cac
Alberto <albertolagoalvarado@gmail.com>
2012-03-29 14:27:23 +0200

    Foros_terminados

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,19 +3,30 @@ class CommentsController < ApplicationController
   def create
     
     @comment = Comment.new(params[:comment])
-    @commentable = Comment.find(params[:commentable_id])
+    @commentable = find_commentable
     @comment.commentable = @commentable
     @comment.user = current_user
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @idea, notice: 'Comment was successfully created.' }
-        format.json { render json: @idea, status: :created, location: @idea }
+        format.html { redirect_to @commentable, notice: 'Comment was successfully created.' }
+        format.json { render json: @commentable, status: :created, location: @commentable }
       else
-        format.html { render action: @idea }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
+        format.html { render action: @commentable }
+        format.json { render json: @commentable.errors, status: :unprocessable_entity }
       end
     end
   end
+  
+  private
+
+def find_commentable
+  params.each do |name, value|
+    if name =~ /(.+)_id$/
+      return $1.classify.constantize.find(value)
+    end
+  end
+  nil
+end
   
  
 end
